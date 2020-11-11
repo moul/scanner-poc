9fb24d9f5de1dcb946043052c5e68ab7870cf1c5
numidiasoft <tarik@bearer.sh>
2020-11-11 18:09:12 +0100

    manage binaries per platform

--- script.sh
+++ script.sh
@@ -1,29 +1,65 @@
 #!/bin/sh
 
+URL_BASE="https://bearer-hub-backend-production.s3.eu-west-3.amazonaws.com"
+BINARY_NAME="bearer-cli-latest"
 blue="\033[34m"
 reset="\033[0m"
 red="\033[31m"
 green="\033[32m"
+os="$(uname -s | awk '{print tolower($0)}')"
+arch="$(uname -p | awk '{print tolower($0)}')"
 
-warn() {
-  echo -e "$1" >/dev/stderr
-}
+warn() { echo -e "$1" >/dev/stderr ;}
+alert() { warn "$red$1$reset" ;}
+info() { warn "$green$1$reset" ;}
+notice() { warn "$blue$1$reset" ;}
 
-alert() {
-  warn "$red$1$reset"
-}
+if [ $os = 'darwin' ]; then
+  case $arch in
+  "i386")
+    DOWNLOAD_URL="$URL_BASE/$os/386/bin/$BINARY_NAME"
+  ;;
+  "x86_64")
+    DOWNLOAD_URL="$URL_BASE/$os/amd64/bin/$BINARY_NAME"
+  ;;
+  *)
+    notice "Your system ($os => $arch) is not yet supported"
+    exit
+  ;;
+  esac
+fi
 
-info() {
-  warn "$green$1$reset"
-}
+if [ $os = 'linux' ]; then
+  case $arch in
+  "i386")
+    DOWNLOAD_URL="$URL_BASE/$os/386/bin/$BINARY_NAME"
+  ;;
+  "x86_64")
+    DOWNLOAD_URL="$URL_BASE/$os/amd64/bin/$BINARY_NAME"
+  ;;
+  "aarch64")
+    DOWNLOAD_URL="$URL_BASE/$os/arm64/bin/$BINARY_NAME"
+  ;;
+  *)
+    notice "Your system ($os => $arch) is not yet supported"
+    exit
+  ;;
+  esac
+fi
 
-notice() {
-  warn "$blue$1$reset"
-}
 
 usage() {
   notice "Example :  ./bearer-scanner -d <dir1> <dir2>"
 }
+
+downloadBinary() {
+  notice "Download the the bearer-cli binary from $DOWNLOAD_URL to ~/.bearer"
+  mkdir -p ~/.bearer
+  curl $DOWNLOAD_URL --output ~/.bearer/bearer-cli
+  chmod u+x  ~/.bearer/bearer-cli
+}
+
+
 
 if [ $# -lt 1 ];then
    notice "illegal number of parameters"
@@ -41,7 +77,7 @@ while getopts “:h?d:” opt; do
       shift
       repos=("$@");
     ;;
-    * ) echo "Example :  ./bearer-scanner <dir1> <dir2>"
+    * ) usage
         exit 1
   esac
 done
@@ -61,9 +97,9 @@ mkdir -p $tmpDir
 for repo in "${repos[@]}";do
     realPath="$(cd $repo;pwd)"
 
-    outputName="$(echo $repo | tr "/" "-" )"
-    # TODO replace later with a call to the binary
-    go run ./cli.go $realPath > "$tmpDir/$outputName.json"
+    outputName="$(echo ${realPath:1} | tr "/" "-" )"
+    downloadBinary
+    ~/.bearer/bearer-cli $realPath > "$tmpDir/$outputName.json"
     rm -f bearer-cli.zip
     zip -r -j bearer-cli.zip /tmp/bearer-scanner/*
     notice "new report generated on => $(pwd)/bearer-cli.zip"
